9c848ff01a12e8a34eef13b89094f18b29699781
Aniket Mishra <aniket.mishra200510@gmail.com>
2025-07-21 18:05:32 +0530

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -48,7 +48,7 @@ export default function AboutProject() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.1 }}
             >
-              Hi, I&apos;m{" "}
+              Hi, I'm{" "}
               <span className="text-cyan-400 font-bold animate-pulse">
                 {siteConfig.author.name}
               </span>{" "}
